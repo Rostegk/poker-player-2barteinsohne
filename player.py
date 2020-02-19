32c01c2a6bc20e3dc850239c48af01fbf312b029
Daniel Elsner <daniel.elsner@paessler.com>
2020-02-19 12:28:23 +0100

    fold test

--- player.py
+++ player.py
@@ -6,7 +6,21 @@ class Player:
     def betRequest(self, game_state):
         sys.stderr.write(str(game_state))
         playerId = game_state["in_action"]
-        betSum = game_state["current_buy_in"] - game_state["players"][playerId]["bet"]
+        player = game_state["players"][playerId]
+        card1 = player["whole_cards"][0]
+        card2 = player["whole_cards"][1]
+        numberCards = [2,3,4,5,6,7,8,9]
+        betSum = game_state["current_buy_in"] - player["bet"]
+
+        if card1 == card2:
+            return betSum
+
+        if card1 in numberCards:
+            return 0
+        
+        if card2 in numberCards:
+            return 0
+            
         return betSum
 
     def showdown(self, game_state):
