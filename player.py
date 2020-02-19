13941eea0414339e9065e7bfe538f65b96b7ae47
Robin Stegk <robin.stegk@paessler.com>
2020-02-19 11:45:27 +0100

    test

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        sys.stderr.write(game_state)
+        sys.stderr.write(str(game_state))
         
         return 10
 
