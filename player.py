44a9338133c2c55612470277a113f4c121f32650
Daniel Elsner <daniel.elsner@paessler.com>
2020-02-19 12:19:46 +0100

    fix all

--- player.py
+++ player.py
@@ -5,8 +5,8 @@ class Player:
 
     def betRequest(self, game_state):
         sys.stderr.write(str(game_state))
-        inAction = game_state["in_action"]
-        betSum = game_state["current_buy_in"] - game_state[game_state["players"][inAction]["bet"]
+        playerId = game_state["in_action"]
+        betSum = game_state["current_buy_in"] - game_state["players"][playerId]["bet"]
         return betSum
 
     def showdown(self, game_state):
