05c98349227c3d1e3f6130ef869e9e245d01a923
Daniel Elsner <daniel.elsner@paessler.com>
2020-02-19 10:41:03 +0100

    print test

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print "HELLOOOO"
         return 10
 
     def showdown(self, game_state):
